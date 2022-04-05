2f8fa6cd9637863c92d8f6e9482f2bc327c78c13
ashraf <ashrafeid@outlook.com>
2022-04-05 11:22:33 +0300

    deploy workflow test

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -3,7 +3,8 @@ import axios from 'axios'
 const request = axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
     params: {
-        key: process.env.REACT_APP_YOUTUBE_API_KEY1
+        // key: process.env.REACT_APP_YOUTUBE_API_KEY1
+        key:"AIzaSyBcRw2qqx_uVDXAwiWtXVL-lR7wetMhsg8"
     }
 })
 export default request
