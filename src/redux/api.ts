470a001053be3f8281c01f0a1ed0d25a70e9b888
ashraf <ashrafeid@outlook.com>
2022-04-05 11:49:51 +0300

    ready for deploy

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -3,8 +3,7 @@ import axios from 'axios'
 const request = axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
     params: {
-        // key: process.env.REACT_APP_YOUTUBE_API_KEY1
-        key:"AIzaSyBcRw2qqx_uVDXAwiWtXVL-lR7wetMhsg8"
+        key: process.env.REACT_APP_YOUTUBE_API_KEY1
     }
 })
 export default request
